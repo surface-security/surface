37c06d28dd1d84c9b20640210d20db6d3e6ce829
pintof2 <fabio.pinto@blip.pt>
2022-11-29 10:34:21 +0000

    reformats files

--- surface/dns_ips/migrations/0003_alter_dnsrecord_tla.py
+++ surface/dns_ips/migrations/0003_alter_dnsrecord_tla.py
@@ -7,14 +7,21 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('inventory', '0001_initial_20211102'),
-        ('dns_ips', '0002_initial'),
+        ("inventory", "0001_initial_20211102"),
+        ("dns_ips", "0002_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='dnsrecord',
-            name='tla',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='dns', to='inventory.application', verbose_name='Application'),
+            model_name="dnsrecord",
+            name="tla",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="dns",
+                to="inventory.application",
+                verbose_name="Application",
+            ),
         ),
     ]
