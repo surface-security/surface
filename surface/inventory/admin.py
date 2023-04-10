1a5b93be1e5449af31d042a6d085a4c166d70289
Bogdan Oniga <bogdan@oniga.me>
2023-04-10 16:39:25 +0200

    feat: add Integration module

--- surface/inventory/admin.py
+++ surface/inventory/admin.py
@@ -3,6 +3,19 @@ from django.contrib.admin.decorators import register
 from . import models
 
 
+@register(models.Integration)
+class IntegrationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'description', 'actions')
+    search_fieds = ('name', 'description', 'actions')
+    list_filter = ('type', )
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request):
+        return False
+
+
 @register(models.Application)
 class ApplicationAdmin(admin.ModelAdmin):
     """
--- surface/inventory/models.py
+++ surface/inventory/models.py
@@ -5,6 +5,28 @@ class Person(models.Model):
     name = models.CharField(max_length=128)
 
 
+class Integration(models.Model):
+    class IntegrationType(models.TextChoices):
+            aws = 'AWS'
+            cloudflare = 'Cloudflare'
+            gcp = 'GCP'
+            github = 'Github'
+
+    name = models.CharField(max_length=255, null=False, blank=False)
+    type = models.CharField(
+        max_length=64,
+        choices=IntegrationType.choices,
+        verbose_name="Integration Type",
+        db_index=True,
+        editable=False
+    )
+    description = models.TextField(null=True, blank=True)
+    actions = models.JSONField(null=False, blank=False)
+
+    def __str__(self) -> str:
+         return f'{self.name} ({self.type})'
+
+
 class Application(models.Model):
     tla = models.CharField(max_length=128, blank=True, null=True, db_index=True)  # three/ten letter acronym
     managed_by = models.ForeignKey('Person', blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
