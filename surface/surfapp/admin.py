5eba516d38e4e3f15b17c434148f180c18410ef1
Gustavo Silva <gustavosantaremsilva@gmail.com>
2023-04-10 22:14:10 +0100

    feat: install django-impersonator (#111)
    Add ability to impersonator non-admin users by admin users, both to work
    as an example but also to help debug user and group permissions.

--- surface/surfapp/admin.py
+++ surface/surfapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib.admin import site
+from django.contrib.auth import admin
+from impersonate.admin import impersonate_action
 
+admin.UserAdmin.actions.append(impersonate_action)
 
 site.site_title = 'Surface'
 site.site_url = 'https://github.com/surface-security/surface'
