ade88bc37d08c620e1e46b916ea7ad04c8816d78
Fábio Pinto <fabio.pinto@blip.pt>
2025-10-21 15:01:46 +0100

    fixes test

--- e2e/test_dkron.py
+++ e2e/test_dkron.py
@@ -13,17 +13,11 @@ TEST_IO = {
 @pytest.mark.webtest
 def test_dkron_admin_views(prep_dkron, test_cfg, live_server):
     test_cfg.driver.get(f"{live_server.url}/login/")
-    assert "Login" in test_cfg.driver.title
+    assert "Log in" in test_cfg.driver.title
 
-    test_cfg.driver.find_element(by=By.ID, value="id_username").send_keys(
-        test_cfg.username
-    )
-    test_cfg.driver.find_element(by=By.ID, value="id_password").send_keys(
-        f"{test_cfg.password}"
-    )
-    test_cfg.driver.find_element(
-        by=By.XPATH, value='//button[text()="Submit"]'
-    ).send_keys(Keys.ENTER)
+    test_cfg.driver.find_element(by=By.ID, value="id_username").send_keys(test_cfg.username)
+    test_cfg.driver.find_element(by=By.ID, value="id_password").send_keys(f"{test_cfg.password}")
+    test_cfg.driver.find_element(by=By.XPATH, value='//button[text()="Submit"]').send_keys(Keys.ENTER)
 
     assert "Home | Surface" == test_cfg.driver.title
 
@@ -31,15 +25,9 @@ def test_dkron_admin_views(prep_dkron, test_cfg, live_server):
     assert "Add job | Surface" == test_cfg.driver.title
 
     test_cfg.driver.find_element(by=By.ID, value="id_name").send_keys(TEST_IO["name"])
-    test_cfg.driver.find_element(by=By.ID, value="id_schedule").send_keys(
-        TEST_IO["schedule"]
-    )
-    test_cfg.driver.find_element(by=By.ID, value="id_command").send_keys(
-        TEST_IO["command"]
-    )
-    test_cfg.driver.find_element(by=By.ID, value="id_description").send_keys(
-        TEST_IO["description"]
-    )
+    test_cfg.driver.find_element(by=By.ID, value="id_schedule").send_keys(TEST_IO["schedule"])
+    test_cfg.driver.find_element(by=By.ID, value="id_command").send_keys(TEST_IO["command"])
+    test_cfg.driver.find_element(by=By.ID, value="id_description").send_keys(TEST_IO["description"])
     test_cfg.driver.find_element(by=By.ID, value="id_use_shell").click()
     test_cfg.driver.find_element(by=By.ID, value="id_notify_on_error").click()
 
