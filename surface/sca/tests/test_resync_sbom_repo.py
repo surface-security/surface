56276b05cd86dff8073de47fc44e7ce42fed8044
Fábio Pinto <fabio.pinto@blip.pt>
2025-10-30 15:03:16 +0000

    fixes tests

--- surface/sca/tests/test_resync_sbom_repo.py
+++ surface/sca/tests/test_resync_sbom_repo.py
@@ -21,7 +21,7 @@ class Test(TestCase):
     def test_resync_sbom_repo(self, now):
         responses.add(
             responses.GET,
-            f"{settings.SCA_SBOM_REPO_URL}/urn:uuid:46d764e2-aae1-4f82-b9f1-c616308e921d?vuln_data=True",
+            f"{settings.SCA_SBOM_REPO_URL}/v1/sbom/urn:uuid:46d764e2-aae1-4f82-b9f1-c616308e921d?vuln_data=True",
             status=200,
             content_type="application/json",
             json=data.sbom_data,
@@ -29,7 +29,7 @@ class Test(TestCase):
 
         responses.add(
             responses.GET,
-            f"{settings.SCA_SBOM_REPO_URL}/all?since={datetime.datetime.strftime(timezone.now() - timezone.timedelta(hours=1), '%Y-%m-%dT%H:%M:%S.%f')}",
+            f"{settings.SCA_SBOM_REPO_URL}/v1/sbom/all?since={datetime.datetime.strftime(timezone.now() - timezone.timedelta(hours=1), '%Y-%m-%dT%H:%M:%S.%f')}",
             status=200,
             content_type="application/json",
             json=["urn:uuid:46d764e2-aae1-4f82-b9f1-c616308e921d"],
